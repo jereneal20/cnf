3b401f47ba88d5873e5dde92b2d3291a67c26c9f
Jereneal <jereneal20@gmail.com>
2016-04-01 17:03:37 +0900

    Creating formula tree

--- cnf/__init__.py
+++ cnf/__init__.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 import os, sys
+from collections import deque
 
+sampleInput = "> & - p q & p > r q"
+
+class Formula:
+    operator = ""
+    left = ""
+    right = ""
+
+    def __init__(self, tokens):
+        token = tokens.popleft()
+        if token == "&" or token == "|" or \
+           token == ">" or token == "<" or token == "=":
+            self.operator = token
+            self.left = Formula(tokens)
+            self.right = Formula(tokens)
+        elif token == "-":
+            self.operator = token
+            self.left = Formula(tokens)
+        else:
+            self.left = token #Atomic
+            # print("Invalid operator or syntax.\n")
+            # exit(1)
 
 
 def main(argv):
-    
+    print(sampleInput)
+
+    originalFormula = Formula(deque(sampleInput.split()))
 
 
 if __name__ == "__main__":
