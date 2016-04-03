2fedddf4eb21dc405db48463cabfd740621ef409
Jereneal <jereneal20@gmail.com>
2016-04-03 23:43:04 +0900

    SAT result dictionary added

---
+++ cnf/MiniSATCaller.py
@@ -0,0 +1,64 @@
+from Formula import Formula
+import subprocess
+
+def createVarDictionary(cnfForm):
+	varDictionary = {}
+	iterNum = 1
+	for token in cnfForm.formulaAsString().split():
+		if token == "&" or token == "|" or token == "-":
+			continue
+		elif token not in varDictionary:
+			varDictionary[token] = iterNum
+			iterNum += 1
+		else:
+			pass
+	return varDictionary
+
+def getMiniSATString(infixForm, varDictionary):
+	minisatStr = "c convert to CNF\np cnf " + str(len(varDictionary))
+	minisatContent = ""
+	iterNum = 1
+	for token in infixForm.split():
+		if token in varDictionary:
+			minisatContent += str(varDictionary[token]) + " "
+		if token == "&":
+			minisatContent += "0\n"
+			iterNum += 1
+		if token == "-":
+			minisatContent += "-"
+	minisatContent += "0"
+	minisatStr += " " + str(iterNum) + "\n" + minisatContent
+	return minisatStr
+
+def getMiniSATResult(miniSATStr):
+	f = open("miniSAT.in", 'w')
+	f.write(miniSATStr)
+	f.close()
+
+	miniSATLog = open("log", "w")
+	subprocess.call(['minisat', 'miniSAT.in', 'miniSAT.out'], stdout=miniSATLog, stderr=miniSATLog)
+	miniSATLog.close()
+
+	f = open("miniSAT.out", 'r')
+	result = f.readline()
+	f.close()
+
+	if result == "SAT\n":
+		return "Not Valid"
+	elif result == "UNSAT\n":
+		return "Valid"
+	else:
+		return "Neither SAT nor UNSAT"
+
+
+def getResultDictionary(f, varDictionary):
+	f.readline()
+	result = f.readline().split()
+
+	dictionary2 = {}
+	for iter in varDictionary:
+		if str(varDictionary[iter]) in result:
+			dictionary2[iter] = "1"
+		else:
+			dictionary2[iter] = "-1"
+	return dictionary2
--- cnf/__init__.py
+++ cnf/__init__.py
@@ -4,97 +4,66 @@ from collections import deque
 
 from Formula import Formula
 import ConvertToCNF as CNF
+import MiniSATCaller as minisat
 
 sampleInput = "> & - p q & p > r q"
 sampleInput2 = "- = - a1 b2"
 sampleInput3 = "= - a1 a1"
 
-def createVarDictionary(cnfForm):
-    varDictionary = {}
-    iterNum = 1
-    for token in cnfForm.formulaAsString().split():
-        if token == "&" or token == "|" or token == "-":
-            continue
-        elif token not in varDictionary:
-            varDictionary[token] = iterNum
-            iterNum += 1
-        else:
-            pass
-    return varDictionary
-
-def getMiniSATString(infixForm, varDictionary):
-    minisatStr = "c convert to CNF\np cnf " + str(len(varDictionary))
-    minisatContent = ""
-    iterNum = 1
-    for token in infixForm.split():
-        if token in varDictionary:
-            minisatContent += str(varDictionary[token]) + " "
-        if token == "&":
-            minisatContent += "0\n"
-            iterNum += 1
-        if token == "-":
-            minisatContent += "-"
-    minisatContent += "0"
-    minisatStr += " " + str(iterNum) + "\n" + minisatContent
-    return minisatStr
-
-def getMiniSATResult(miniSATStr):
-    f = open("miniSAT.in", 'w')
-    f.write(miniSATStr)
-    f.close()
-
-    miniSATLog = open("err.log", "w")
-    subprocess.call(['minisat', 'miniSAT.in', 'miniSAT.out'], stdout=miniSATLog, stderr=miniSATLog)
-    miniSATLog.close()
-
-    f = open("miniSAT.out", 'r')
-    result = f.readline()
-    f.close()
-
-    if result == "SAT\n":
-        return "Not Valid"
-    elif result == "UNSAT\n":
-        return "Valid"
-    else:
-        return "Neither SAT nor UNSAT"
-
 
 
 def main(argv):
-    if len(argv) >= 2:
-        print ("Use \" \" to the propositional formula: Syntax Example: cnf \"> & - p q & p > r q\"")
-        exit(0)
-    elif len(argv) == 0:
-        print ("Input format error: Syntax Example: cnf \"> & - p q & p > r q\"")
-        exit(0)
+	# if len(argv) >= 2:
+	#     print ("Use \" \" to the propositional formula: Syntax Example: cnf \"> & - p q & p > r q\"")
+	#     exit(0)
+	# elif len(argv) == 0:
+	#     print ("Input format error: Syntax Example: cnf \"> & - p q & p > r q\"")
+	#     exit(0)
 
-    print("Input: "+ argv[1])
-    originalFormula = Formula(deque(argv[1].split()))
+	print("Input: "+ sampleInput)
+	originalFormula = Formula(deque(sampleInput.split()))
 
-    conjNormForm = CNF.convertToCNF(originalFormula)
-    print("CNF Form  : " + conjNormForm.formulaAsString())
+	conjNormForm = CNF.convertToCNF(originalFormula)
+	print("CNF Form  : " + conjNormForm.formulaAsString())
 
-    infix = conjNormForm.formulaAsInfixString()
-    print ("Infix Form: "+infix)
+	infix = conjNormForm.formulaAsInfixString()
+	print ("Infix Form: "+infix)
 
-    varDictionary = createVarDictionary(conjNormForm)
-    miniSATStr = getMiniSATString(infix, varDictionary)
-    print(getMiniSATResult(miniSATStr))
+	varDictionary = minisat.createVarDictionary(conjNormForm)
+	miniSATStr = minisat.getMiniSATString(infix, varDictionary)
+	print(minisat.getMiniSATResult(miniSATStr))
+
+	f = open("miniSAT.out", 'r')
+	f.readline()
+	result = f.readline().split()
+	intResult = []
+	for iter in result:
+		intIter = int(iter)
+		if intIter != 0:
+			intResult.append(intIter)
+			pass
 
 
-    # for a in varDictionary:
-    #     print(a)
+	dictionary2 = minisat.getResultDictionary(sampleInput,varDictionary)
 
-    # implFreeFormula = CNF.implicationFree(CNF.equivalanceFree(originalFormula))
-    # print("ImplFree: "+ implFreeFormula.formulaAsString())
+	print(varDictionary)
+	print(result)
+	print(dictionary2)
+
+
+	# for a in varDictionary:
+	#     print(a)
+
+	# implFreeFormula = CNF.implicationFree(CNF.equivalanceFree(originalFormula))
+	# print("ImplFree: "+ implFreeFormula.formulaAsString())
 	#
-    # negFreeForm = CNF.negationNormalForm(implFreeFormula)
-    # print("NegFree:  "+ negFreeForm.formulaAsString())
+	# negFreeForm = CNF.negationNormalForm(implFreeFormula)
+	# print("NegFree:  "+ negFreeForm.formulaAsString())
 	#
-    # conjNorm = CNF.conjunctiveNormalForm(negFreeForm)
-    # print("ConjNorm: " + conjNorm.formulaAsString())
+	# conjNorm = CNF.conjunctiveNormalForm(negFreeForm)
+	# print("ConjNorm: " + conjNorm.formulaAsString())
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
